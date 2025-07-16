221dce4e5adfe446040d497dde8e30f2bcb490de
WSCC-InstructionalDesign <kbiehl@westshore.edu>
2025-07-16 14:44:56 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import StudentTimeManager from './components/StudentTimeManager';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <StudentTimeManager />
+    </div>
+  );
+}
+
+export default App;
